538fdc13c29f905f16ec86243a4abe4f6dd1d6a5
nazeer.tsff <nazeer.tsff@gmail.com>
2021-11-18 11:41:28 +0530

    added size of datatype program

---
+++ TEST1/Sizeofdatatype.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a;
+
+cout<<"size of int  "<< sizeof(a)<<endl;
+
+float b;
+
+cout<<"size of float  "<< sizeof(b)<<endl;
+
+
+double d;
+
+cout<<"size of double  "<< sizeof(d)<<endl;
+
+}
