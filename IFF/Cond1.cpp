b8724bae11fb0412c1168c0cb56625816dc963d3
nazeer.tsff <nazeer.tsff@gmail.com>
2021-11-18 20:06:08 +0530

    added if condition program

---
+++ IFF/Cond1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int savings;
+    cin >> savings;
+
+    if (savings > 5000){
+        
+        //if condition inside if condn
+
+          if (savings > 10000)  {
+                cout << "shopping with Nisha" << endl;
+            }
+            else
+            {
+                cout << "Movie with Nisha" << endl;
+            }
+    }
+    else if (savings>2000){
+
+                cout << "coffee with Neha" << endl;
+                 
+        }
+
+    else {
+           cout << "friends" << endl;
+
+        }
+        
+     }      
+
---
+++ TEST1/addnumbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int n1;
+int n2;
+int sum1;
+
+cin>>n1;
+
+cin>>n2;
+
+sum1 = n1+n2;
+
+cout<<sum1<<endl;
+
+return 0;
+
+
+}
