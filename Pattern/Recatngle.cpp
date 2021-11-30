055d42b0ba4e8026b33aad5086b5dccf467d4935
nazeer.tsff <nazeer.tsff@gmail.com>
2021-11-30 21:29:26 +0530

    added pattern program

---
+++ Pattern/Recatngle.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int rows, cols;
+
+    cout << "Enter number of rows and columns"<<endl;
+
+    cin >>rows >>cols;
+
+    for (int i = 0; i < rows; i++)
+    {
+
+        for (int j = 0; j < cols; j++)
+        {
+
+            cout <<"*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
