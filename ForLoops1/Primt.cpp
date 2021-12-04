3ae059c9edf94dfadec7c4d99041452f1229b34f
nazeer.tsff <nazeer.tsff@gmail.com>
2021-12-04 07:23:07 +0530

    prime program modified

--- ForLoops1/Primt.cpp
+++ ForLoops1/Primt.cpp
@@ -1,30 +1,20 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
-
     cout << "Enter number" << endl;
-
     int n;
     cin >> n;
-
     bool flag = 0;
-
     for (int i = 2; i < n; i++)
     {
-
         if (n % i == 0)
         {
-
             cout << "Non Prime number" << endl;
-            bool flag = 1;
-
+            flag = 1;
             break;
         }
     }
-
     if (flag == 0)
-
         cout << "Prime number" << endl;
 }
