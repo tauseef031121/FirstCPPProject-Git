a498ce221d077cdeaa789e8b6699ce3b9dfed551
nazeer.tsff <nazeer.tsff@gmail.com>
2022-03-12 18:53:20 +0530

    change

---
+++ LinearSearch/test.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 270;
+    int b = 40;
+
+    if (a < b)
+    {
+
+        cout << "pass" << endl;
+    }
+
+    else
+    {
+        cout << "fail" << endl;
+    }
+}
+
